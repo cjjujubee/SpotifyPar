e67a78c7d5661e7fc6c77cb9a5aedb5026ae895d
cjjujubee <connie.jew@gmail.com>
2016-05-06 15:40:33 -0700

    Trying to get related artist--WIP

--- index.js
+++ index.js
@@ -3,13 +3,16 @@
     var events = require('events');
 
     var getFromApi = function(endpoint, args) {
+        // console.log('Where is Adele');
         var emitter = new events.EventEmitter();
         unirest.get('https://api.spotify.com/v1/' + endpoint)
             .qs(args)
             .end(function(response) {
                 if (response.ok) {
+                    // console.log(emitter);
                     emitter.emit('end', response.body);
                 } else {
+                    console.log('I am an error');
                     emitter.emit('error', response.code);
                 }
             });
@@ -27,55 +30,31 @@
         });
 
         searchReq.on('end', function(item) {
+            // console.log('this is the item: ' + item);
             var artist = item.artists.items[0];
             var artistid = item.artists.items[0].id;
-            var searchReqid = getFromApi('artists', {
-                id: artistid,
-                limit: 3,
-                type: 'artist'
-            });
-            searchReqid.on('end', function(iditem) {
-                res.json(iditem);
-            });
+            searchReq.emit('getRelatedArtists', artist);
+        })
+
+        // var relArtist = getFromAPI(blah);
+        //
+        // relArtist.on('end'.....)
 
 
-            // searchReq.on('end', function(item) {
-            // var artistid = item.artists.items[0].id;
-            //     res.json(artistid);
-            // });
+
+        searchReq.on('getRelatedArtists', function (artist) {
+          var relatedRequest = getFromApi('artists/'+artist.id+'/related-artists');
+          console.log(relatedRequest);
+          relatedRequest.on('end', function(item) {
+              console.log(item);
+              res.json(item);
+          })
+          //
+        })
+
 
             searchReq.on('error', function(code) {
                 res.sendStatus(code);
             });
-        });
     });
     app.listen(8080);
-
-
-    // var events = require('events');
-    //
-    // var progressBar = new events.EventEmitter();
-    //
-    // progressBar.on('onStart', function(count) {
-    //   console.log('Started at: ' + count);
-    // });
-    //
-    // progressBar.on('onProgress', function(count) {
-    //   console.log('You are currently at: ' + count + '%');
-    // });
-    //
-    // progressBar.on('onEnd', function(count) {
-    //   console.log('All done at ' + count + '!');
-    // });
-    //
-    // for (var i = 0; i <= 100; i++) {
-    //     if (i === 0) {
-    //       progressBar.emit('onStart', i);
-    //     }
-    //     else if (i === 100) {
-    //       progressBar.emit('onEnd', i);
-    //     }
-    //     else if (i % 10 === 0) {
-    //       progressBar.emit('onProgress', i);
-    //     }
-    // }
