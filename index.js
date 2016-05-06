98804b07e7b7302d3703fb0816ad0c9e1bd43c2a
OoogleBoogle <simattfield@gmail.com>
2016-05-06 10:40:05 -0700

    Fixed progress thing

--- index.js
+++ index.js
@@ -3,20 +3,20 @@
 // onStart callback, and begin count from 1 to 100. Every 10 items it counts it should call the onProgress callback,
 // providing how far along it is as an argument. Finally it should call the onEnd callback.
 
-var progressBar = function(secondsPast, callback) {
-  setTimeout(function() {
-    callback();
-  }, secondsPast * 1000)
-};
+// var progressBar = function(secondsPast, callback) {
+//   setTimeout(function() {
+//     callback();
+//   }, secondsPast * 1000)
+// };
 
 // call progressBar 'start' with no seconds a call back => logs something
   // nested callbacks inside? nested timeouts??
-var progressBar = 0;
-var onStart = function() {
-  for (var i = 0; i <= 100; i++) {
-    var onProgress = function(progress, callback) {
-      setTimeout(function)
-    }
+// var progressBar = 0;
+// var onStart = function() {
+//   for (var i = 0; i <= 100; i++) {
+//     var onProgress = function(progress, callback) {
+//       setTimeout(function)
+//     }
 
     // if (i === 0) {
     //   progressBar(0, function() {
@@ -29,22 +29,31 @@ var onStart = function() {
     // else if (i === 100) {
     //   onEnd
     // }
-  }
-}
+//   }
+// }
 
 var progress = function(onStart, onProgress, onEnd) {
   var i;
   for (i = 0; i <= 100; i++) {
-    progress(function(start) {
-      // on start
-    }, function(progress) {
-      // on P
-    }, function(end) {
-      // on end
-    });
+    if (i === 0) {
+      onStart(i);
+    }
+    else if (i === 100) {
+      onEnd(i);
+    }
+    else if (i % 10 === 0) {
+      onProgress(i);
+    }
   }
 };
 
+progress(function(start) {
+  console.log(start);
+}, function(middle) {
+  console.log(middle);
+}, function(end) {
+  console.log(end);
+});
 
 
 // var onStart = function(secondsPast, callback) {
